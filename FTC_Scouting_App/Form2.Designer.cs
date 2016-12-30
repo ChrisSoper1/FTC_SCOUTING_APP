07577f3e5c1145647c3481371a880cc16360ed15
Jarred Moyer <jarredds941@gmail.com>
2016-12-29 21:23:48 -0500

    push

--- FTC_Scouting_App/Form2.Designer.cs
+++ FTC_Scouting_App/Form2.Designer.cs
@@ -28,100 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.TeamNumberInput = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.TeamNameInput = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // TeamNumberInput
-            // 
-            this.TeamNumberInput.Location = new System.Drawing.Point(145, 53);
-            this.TeamNumberInput.Name = "TeamNumberInput";
-            this.TeamNumberInput.Size = new System.Drawing.Size(120, 20);
-            this.TeamNumberInput.TabIndex = 0;
-            this.TeamNumberInput.Text = "Enter team number here";
-            this.TeamNumberInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(205, 137);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(54, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Save";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // TeamNameInput
-            // 
-            this.TeamNameInput.Location = new System.Drawing.Point(145, 104);
-            this.TeamNameInput.Name = "TeamNameInput";
-            this.TeamNameInput.Size = new System.Drawing.Size(114, 20);
-            this.TeamNameInput.TabIndex = 2;
-            this.TeamNameInput.Text = "Enter team name here";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(23, 104);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 20);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Team Name";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 53);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 20);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Team Number";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(84, 9);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 20);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Reset Array";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(505, 53);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(173, 20);
-            this.textBox3.TabIndex = 8;
-            this.textBox3.Text = "Add competition";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(684, 53);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // comboBox1
             // 
+            this.comboBox1.DisplayMember = "compList.location";
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(477, 178);
+            this.comboBox1.Items.AddRange(new object[] {
+            "saasd"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(231, 21);
-            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Size = new System.Drawing.Size(179, 21);
+            this.comboBox1.TabIndex = 0;
             // 
             // Form2
             // 
@@ -129,31 +48,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(944, 554);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.TeamNameInput);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.TeamNumberInput);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox TeamNumberInput;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox TeamNameInput;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- FTC_Scouting_App/Form2.cs
+++ FTC_Scouting_App/Form2.cs
@@ -21,36 +21,6 @@ namespace FTC_Scouting_App
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            String check = TeamNumberInput.Text;
-            
-            
-
-        }
-
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-            
-
-                 
-        }
+        
     }
 }
